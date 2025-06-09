cc26bc211b56e5bda68f7c95d8e84bbc9c047d74
hula-hh <christophernathanaelnnnael@gmail.com>
2025-06-09 23:54:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const pages = [
   "Hai Bianca",
   "Aku mau jujur sebenarnya...",
   "Aku kangen banget sama kamu. Aku nyariin letak rumahmu. Dan ketika web ini kamu baca berarti aku udah nyerah nyari :). Aku cuman pengen kita jadi temen lagi, trus juga sebenarnya ketika aku nemuin akunmu, kenangan-kenangan lama itu muncul. Lalu hal itu yang membuat aku buat ngechat atau ngedm kamu. Oh iya aku sebenarnya takut buat ngechat dan semua chattan yang aku kirim itu buatan ai :). Lucu ya? Atau aneh? Ya memang seperti itu adanya.",
-  "Yaudah, itu aja yang mau aku sampein. Oh iya kamu mau jadi temen ku lagi? Jawab di chat ya, yang jujur aja aku gak bakal ngapa-ngapain. Maaf ya aku ngeganggu kamu. Trima kasih udah jadi kisah di hidupku. I Love you ❤️"
+  "Yaudah, itu aja yang mau aku sampein. Oh iya kamu mau jadi temen ku lagi? Jawab di chat ya, yang jujur aja aku gak bakal ngapa-ngapain kok, aku tunggu ya jawabannya. Jangan gak dibales. Maaf ya aku ngeganggu kamu. Trima kasih udah jadi kisah di hidupku. I Love you ❤️"
 ];
 
 let pageIndex = 0;
